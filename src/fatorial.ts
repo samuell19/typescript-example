fc10f16ba58b2adc6c9b9eeed81512a9aaf9b4b1
samuel <samuelmedeiros2025@gmail.com>
2025-06-06 16:23:31 +0000

    feat: condição para não calcular fatorial de número abaixo de zero

--- src/fatorial.ts
+++ src/fatorial.ts
@@ -1,6 +1,6 @@
 export function fatorial(n: number): number {
   if (n < 0) {
-    throw new Error("Fatorial is not defined for negative numbers.");
+    throw new Error("Não é possível calcular o fatorial de um número negativo");	
   }
   if (n === 0 || n === 1) {
     return 1;
