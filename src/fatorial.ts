33caa6ba1c1fc2f32b83444a54bb786bd8e8553f
samuel <samuelmedeiros2025@gmail.com>
2025-06-06 16:40:01 +0000

    fix: ajustando os testes

--- src/fatorial.ts
+++ src/fatorial.ts
@@ -2,6 +2,7 @@ export function fatorial(n: number): number {
   if (n < 0) {
     throw new Error("Fatorial não existe para números negativos");
   }
-  if (n === 0 || n== 1) return 1;
+  if (n === 0 || n== 1) 
+    return 1;
   return n * fatorial(n - 1);
 }
